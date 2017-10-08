c7b28274b3b14338d88bfe7dcba53ffa7e2b4e20
Kai T. Ohlhus <k.ohlhus@gmail.com>
2017-10-08 03:05:32 +0200

    Remove unused file.

--- generate_qr_code.m
+++
@@ -1,8 +0,0 @@
-function generate_qr_code(str)
-## Load zxing library
-javaaddpath ("zxing_qrcode.jar");
-
-encoder = javaObject ("qrcode.QREncoder");
-java_get ("qrcode.QREncoder", "encode")
-javaMethod ("encode", "qrcode.QREncoder", "Hallo");
-endfunction
